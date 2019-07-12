b60ff5b4ec053637861624c64a57a4b730be4b62
Peter Stavny <peter.stavny@wirecard.com>
2019-07-12 14:51:28 +0200

    Release 3.5.1.

--- eCom/WDeCom.framework/Headers/WDECPaymentMethodName.h
+++ eCom/WDeCom.framework/Headers/WDECPaymentMethodName.h
@@ -39,4 +39,21 @@ typedef NS_ENUM (NSUInteger, WDECPaymentMethodName) {
     WDECPaymentMethodNameTotalNumber
 };
 
+/**
+ @brief Converts payment method string representation to WDECPaymentMethodName enumeration
+ 
+ @param code transaction type string representation
+ 
+ @return WDECPaymentMethodName enumeration
+ */
+WDECPaymentMethodName WDECPaymentMethodNameFromCode(NSString *code);
+/**
+ @brief Converts transaction type WDECPaymentMethodName enumeration to string representation
+ 
+ @param paymentMethodName transaction type WDECPaymentMethodName enumeration
+ 
+ @return transaction type string representation
+ */
+NSString *WDECPaymentMethodNameGetCode(WDECPaymentMethodName paymentMethodName);
+
 /** @} */
